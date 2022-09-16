58ec8b8e45a8c578de913c5594324eadcdb11710
nedoxff <nedoprogrammer0xff@gmail.com>
2022-09-17 01:25:16 +0300

    ✨ start working on rooms

---
+++ Api/Rooms.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+using TermiunoServer.Models;
+
+namespace TermiunoServer.Api;
+
+public partial class GameHub
+{
+    private static JsonSerializerSettings _camelCaseSettings = new()
+    {
+        ContractResolver = new CamelCasePropertyNamesContractResolver()
+    };
+    
+    public static List<Room> Rooms = new();
+    public async Task CreateRoom(string name, int maxPlayers)
+    {
+        var room = new Room
+        {
+            Id = Guid.NewGuid().ToString("N"),
+            MaxPlayers = maxPlayers,
+            Playing = false,
+            Name = name,
+            CreatorId = Context.UserIdentifier!
+        };
+        Log.Information("Created new room ({Id})", room.Id);
+        Rooms.Add(room);
+        await Clients.All.SendAsync("RefreshRoomsList", JsonConvert.SerializeObject(Rooms, _camelCaseSettings));
+    }
+
+    public async Task GetAndRefreshRooms() => await Clients.Caller.SendAsync("RefreshRoomsList", JsonConvert.SerializeObject(Rooms, _camelCaseSettings));
+}
---
+++ Models/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace TermiunoServer.Models;
+
+public struct Room
+{
+    public string Name = "";
+    public string CreatorId = "";
+    public string Id;
+    public int MaxPlayers = 0;
+    public bool Playing;
+    public Dictionary<string, string> Players = new();
+
+    public Room()
+    {
+    }
+}
